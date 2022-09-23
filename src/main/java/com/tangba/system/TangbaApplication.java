cfe9472a37c78114275189897bb3d86db8ee635e
yixin <927871050@qq.com>
2022-09-23 11:00:31 +0800

    first commit

---
+++ src/main/java/com/tangba/system/TangbaApplication.java
@@ -0,0 +1,13 @@
+package com.tangba.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangbaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/tangba/system/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.tangba.system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "成功访问";
+    }
+}
